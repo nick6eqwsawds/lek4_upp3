65f0d1141edd26e10664acf7cf19cad9b2c8428e
otto hoffman <otto.hoffman@outlook.com>
2023-09-27 10:32:18 +0200

    Lek fält upg 2 skriv in 4 heltal och skrivs omvänt

--- src/App.java
+++ src/App.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        try (Scanner tangentbord = new Scanner(System.in)) {
+        System.out.println("Skriv 4 heltal: ");
+        int[] heltal = new int[4];
 
-        
+        heltal[0] = tangentbord.nextInt();
+        heltal[1] = tangentbord.nextInt();
+        heltal[2] = tangentbord.nextInt();
+        heltal[3] = tangentbord.nextInt();
+
+        System.out.println("dina heltal var i annan ordning: "+heltal[3]+" , "+heltal[2]+" , "+heltal[1]+" och "+heltal[0]);
+
+
+        }
     }
 }
